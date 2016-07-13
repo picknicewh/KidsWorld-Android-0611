1c369f876ffa4e6d9c77ea05731327049ae2fde9
Shurrik9527 <272775600@qq.com>
2016-07-13 15:39:14 +0800

    init

---
+++ baselibrary/src/main/java/net/hunme/baselibrary/Test.java
@@ -0,0 +1,10 @@
+package net.hunme.baselibrary;
+
+/**
+ * Created by Administrator on 2016/7/13.
+ */
+public class Test {
+    public static void print(){
+        System.out.println("123");
+    }
+}
