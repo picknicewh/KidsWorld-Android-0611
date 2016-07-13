8d74a1a5a35246a91bbd5ef74fee7ccfa150e640
Akame <1585241320@qq.com>
2016-07-13 15:58:10 +0800

    update by Akame

--- baselibrary/src/main/java/net/hunme/baselibrary/Test.java
+++ baselibrary/src/main/java/net/hunme/baselibrary/Test.java
@@ -1,10 +1,13 @@
 package net.hunme.baselibrary;
 
+import android.content.Context;
+import android.widget.Toast;
+
 /**
  * Created by Administrator on 2016/7/13.
  */
 public class Test {
-    public static void print(){
-        System.out.println("123");
+    public static void print(Context context){
+        Toast.makeText(context, "-----Test Akame--------", Toast.LENGTH_SHORT).show();
     }
 }
--- message/src/main/java/net/hunme/message/MessageActivity.java
+++ message/src/main/java/net/hunme/message/MessageActivity.java
@@ -3,11 +3,14 @@ package net.hunme.message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import net.hunme.baselibrary.Test;
+
 public class MessageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
+        Test.print(this);
     }
 }
